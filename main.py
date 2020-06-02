84ae939e2f4f0285ee2e3905f652ee2fa2ca3fa6
hugodmorais <hugodanielmorais@gmail.com>
2020-06-02 23:04:32 +0100

    save data on users.json

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+import json
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
@@ -14,8 +16,15 @@ class RootWidget(ScreenManager):
 
 class SignUpScreen(Screen):
   def add_user(self, uname, pword):
-    print(uname, pword)
-  
+    with open("users.json") as file:
+      users = json.load(file)
+    
+    users[uname] = {'username': uname, 'password': pword,
+        'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+
+    with open("users.json", 'w') as file:
+      json.dump(users, file)
+
 class MainApp(App):
   def build(self):
     return RootWidget()
